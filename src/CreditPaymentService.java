8ff9941acafed99f370163844f92750c640c9321
DS <mc_sd@bk.ru>
2023-04-13 15:15:56 -0300

    reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double creditSum, double months, double percent) {
-        double percentMonth = percent /100/12 ;
-        double coefficient = percentMonth*(Math.pow(1+percentMonth,months))/(((Math.pow(1+percentMonth,months))-1));
+        double percentMonth = percent / 100 / 12;
+        double coefficient = percentMonth * (Math.pow(1 + percentMonth, months)) / (((Math.pow(1 + percentMonth, months)) - 1));
         double myCredit = creditSum * coefficient;
         return (int) myCredit;
     }
